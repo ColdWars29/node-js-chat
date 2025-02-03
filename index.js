c4c8cd0105ee12fcd41a6fba5043a519bf67ddf7
ColdWars29 <29GariglioSe@fortcherry.org>
2025-02-03 11:51:52 -0500

    server fix

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketIo = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
 const port = process.env.PORT || 8080;
 
+// Serve static files from the 'public' folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(port, () => {
+// Socket.io connection handling
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg); // Broadcast message to all clients
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+// Start the server
+server.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
